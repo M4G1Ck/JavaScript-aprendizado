b7e48f90e487d76b4f4823da5ff6e22a2ad23387
Gabriel Macedo Araujo <79328112+M4G1Ck@users.noreply.github.com>
2021-06-21 13:53:10 -0300

    PastaObjetoDesestruturacao

---
+++ ObjetosDesestruturacao/index.js
@@ -0,0 +1,12 @@
+const pessoas = {
+    nome: 'Gabriel',
+    sobrenome: 'Macedo',
+    idade: 19,
+    endereco: {
+        rua: 'Rua Augusto Severo',
+        numero: 841
+    }
+};
+
+const {  nome  } 
+console.log(nome);
