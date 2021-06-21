e1d29c06f8bb82715a22edba7a9cd60030591d57
Gabriel Macedo Araujo <79328112+M4G1Ck@users.noreply.github.com>
2021-06-21 14:05:35 -0300

    DesestruturacaoEmObjetos

--- ObjetosDesestruturacao/index.js
+++ ObjetosDesestruturacao/index.js
@@ -1,12 +1,28 @@
-const pessoas = {
+const pessoa = {
     nome: 'Gabriel',
     sobrenome: 'Macedo',
     idade: 19,
     endereco: {
-        rua: 'Rua Augusto Severo',
+        //rua: 'Rua Augusto Severo',
         numero: 841
     }
 };
 
-const {  nome  } 
-console.log(nome);
+
+// desestruturacao
+// mudando o nome da variavel; precisa comentar o nome
+// const {  nome: n = 'laulau', sobrenome, idade  } = pessoa;
+// console.log(n, sobrenome);
+
+// const {  
+//     endereco: { rua, numero },
+//     endereco
+//   } = pessoa;
+// console.log(rua, numero, endereco);
+
+// mudando o valor da variavel; precisa comentar a rua
+const {  
+    endereco: { rua: r = 12345, numero },
+    endereco
+  } = pessoa;
+console.log(r, numero, endereco);
